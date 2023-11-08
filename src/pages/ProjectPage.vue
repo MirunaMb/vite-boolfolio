b98b4b67c963516a5cbe3f32b4f3dda755c9a3a3
MirunaMb <mirunamarcela98@gmail.com>
2023-11-08 18:23:24 +0100

    create projectpage

---
+++ src/pages/ProjectPage.vue
@@ -0,0 +1,37 @@
+<script>
+import ProjectCard from "../components/projects/ProjectCard.vue";
+import axios from "axios";
+import { store } from '../data/store';
+export default {
+    data() {
+        return {
+            project: Object,
+            title: "Dettaglio project"
+        };
+    },
+
+    components: {
+        ProjectCard,
+    },
+    created() {
+        console.log(store.api.baseUrl + 'projects/' + this.$route.params.id)
+        axios.get(store.api.baseUrl + 'projects/' + this.$route.params.id)
+            .then((response) => {
+                this.project = response.data;
+                console.log(this.project);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+};
+</script>
+
+<template>
+    <div class="container my-5">
+        <h1 class="my-5">Dettaglio project</h1>
+        <ProjectCard :project="project" />
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
