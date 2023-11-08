ba9e012261c1da414dfdab4f09a66ecc945c9b10
MirunaMb <mirunamarcela98@gmail.com>
2023-11-08 22:08:13 +0100

    set is isDetail,put image

--- src/components/projects/ProjectCard.vue
+++ src/components/projects/ProjectCard.vue
@@ -1,32 +1,21 @@
 <script>
-// import MyComponent from "./components/MyComponent.vue";
-
-export default {
-    data() {
-        return {
-
-        };
-    },
-    props: {
-        project: Object
-    },
-
-    // components: {
-    //   MyComponent,
-    // },
-    methods: {
-        getBadgeTechnology(technology) {
-            //   come dare colore a technology
-            if (technology.name_technologies === 'Tecnologia1') {
-                return 'badge-red';
-            } else if (technology.name_technologies === 'Tecnologia2') {
-                return 'badge-blue';
-            } else {
-                return 'badge-green';
-            }
+    export default {
+        props: {
+            project: Object,
+            isDetail: Boolean,
         },
-    },
-};
+        methods: {
+            getBadgeTechnology(technology) {
+                if (technology.name_technologies === 'Tecnologia1') {
+                    return 'badge-red';
+                } else if (technology.name_technologies === 'Tecnologia2') {
+                    return 'badge-blue';
+                } else {
+                    return 'badge-green';
+                }
+            },
+        },
+    };
 </script>
 
 <template>
@@ -34,43 +23,44 @@ export default {
         <div class="card h-100">
             <div class="card-header d-flex justify-content-between align-items-start">
                 <span class="badge" :style="{ backgroundColor: project.type.color }">
-                    {{ project.type.name }}
+                    {{ project . type . name }}
                 </span>
 
                 <span class="badge" v-for="technology in project.technologies" :class="getBadgeTechnology(technology)">
-                    {{ technology.name_technologies }}
+                    {{ technology . name_technologies }}
                 </span>
             </div>
             <div class="card-body">
-                <h4>{{ project.title }}</h4>
+                <h4>{{ project . title }}</h4>
+                <img v-if="project.cover_image" :src="project.cover_image" class="img-fluid mb-1 rounded" />
             </div>
-
-            <div class="card-footer">
-                <router-link :to="{
-                    name: 'project-detail',
-                    params: {
-                        id: project.id
-                    }
-                }" class="btn btn-primary">Vedi</router-link>
+            <div class="card-footer" v-if="!isDetail">
+                <router-link
+                    :to="{
+                        name: 'project-detail',
+                        params: {
+                            id: project.id
+                        }
+                    }"
+                    class="btn btn-primary">Vedi</router-link>
             </div>
-
         </div>
     </div>
 </template>
-  
+
 <style lang="scss" scoped>
-.badge-red {
-    background-color: red;
-    color: white;
-}
+    .badge-red {
+        background-color: red;
+        color: white;
+    }
 
-.badge-blue {
-    background-color: blue;
-    color: white;
-}
+    .badge-blue {
+        background-color: blue;
+        color: white;
+    }
 
-.badge-green {
-    background-color: green;
-    color: white;
-}
+    .badge-green {
+        background-color: green;
+        color: white;
+    }
 </style>
