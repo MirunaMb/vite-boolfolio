0bbdc86c1f4271fed9393dd71da85fec532b3e6a
MirunaMb <mirunamarcela98@gmail.com>
2023-11-10 21:27:28 +0100

    add router link

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import HomePage from '../pages/HomePage.vue';
 import BlogPage from '../pages/BlogPage.vue';
 import ProjectPage from '../pages/ProjectPage.vue';
 import BlogTypePage from '../pages/BlogTypePage.vue';
+import AdvancedFilterPage from '../pages/AdvancedFilterPage.vue';
 
 const router = createRouter({
     history: createWebHistory(),
@@ -26,6 +27,11 @@ const router = createRouter({
             path: '/blog/type/:type_id',
             name: 'blog-type',
             component: BlogTypePage,
+        },
+        {
+            path: '/advanced-filter',
+            name: 'advanced-filter',
+            component: AdvancedFilterPage,
         }
     ]
 });
