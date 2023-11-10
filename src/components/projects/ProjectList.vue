5b4363a04b13f284bbc0e7d2ddc94d63aa526a40
MirunaMb <mirunamarcela98@gmail.com>
2023-11-10 21:28:15 +0100

    add axios request

--- src/components/projects/ProjectList.vue
+++ src/components/projects/ProjectList.vue
@@ -2,6 +2,7 @@
 import ProjectCard from './ProjectCard.vue';
 import { store } from '../../data/store';
 import axios from 'axios';
+
 export default {
   props: {
     type_id: {
@@ -10,6 +11,7 @@ export default {
   },
   data() {
     return {
+      types: [],  // Corretta la dichiarazione del nome della proprietà
       projects: [],
       api: {
         baseUrl: 'http://127.0.0.1:8000/api/',
@@ -21,14 +23,11 @@ export default {
       },
     };
   },
-
-
   components: {
     ProjectCard,
   },
-
-
   created() {
+    this.fetchTypes();  // Richiama il metodo corretto
     this.fetchProjects();
   },
   methods: {
@@ -36,9 +35,16 @@ export default {
       axios.get(uri)
         .then((response) => {
           this.projects = response.data.data;
-          // this.pagination.prev = response.data.prev_page_url;
-          // this.pagination.next = response.data.next_page_url;
           this.pagination.links = response.data.links;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+    fetchTypes() {
+      axios.get(store.api.baseUrl + 'type')
+        .then((response) => {
+          this.types = response.data;
         })
         .catch((error) => {
           console.error(error);
@@ -52,28 +58,33 @@ export default {
         this.api.baseUrl + 'projects';
     },
   },
-
-
-
 };
 </script>
-
 <template>
-  <div class="container">
-    <h2>ProjectList</h2>
-    <div class="row row-cols-3 g-4">
-      <ProjectCard v-for="project in projects" :isDetail="false" :project="project" />
-    </div>
-  </div>
-  <nav aria-label="Page navigation example">
-    <ul class="pagination">
-      <li v-for="link in pagination.links" :key="link.label" @click="fetchProjects(link.url)" class="page-item">
-        <a class="page-link" href="#" v-html="link.label"></a>
-        <!-- v-htlm interpreta html in vue -->
+  <div>
+    <router-link v-for="type in types" :key="type.id" :to="{
+      name: 'blog-type',
+      params: {
+        type_id: type.id
+      },
+    }" class="badge mx-1" :style="{ backgroundColor: type.color }">
+      {{ type.name }}
+    </router-link>
 
-      </li>
-    </ul>
-  </nav>
+    <div class="container">
+      <h2>ProjectList</h2>
+      <div class="row row-cols-3 g-4">
+        <ProjectCard v-for="project in projects" :key="project.id" :isDetail="false" :project="project" />
+      </div>
+    </div>
+    <nav aria-label="Page navigation example">
+      <ul class="pagination">
+        <li v-for="link in pagination.links" :key="link.label" @click="fetchProjects(link.url)" class="page-item">
+          <a class="page-link" href="#" v-html="link.label"></a>
+        </li>
+      </ul>
+    </nav>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
