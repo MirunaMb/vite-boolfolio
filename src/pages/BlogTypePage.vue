e17e17023827188e4075e05cc7bf5fc0c745e435
MirunaMb <mirunamarcela98@gmail.com>
2023-11-10 21:27:14 +0100

    add method for axios request

--- src/pages/BlogTypePage.vue
+++ src/pages/BlogTypePage.vue
@@ -14,16 +14,28 @@ export default {
 
     components: { ProjectList },
 
-
-    created() {
-        axios.get(store.api.baseUrl + 'type/' + this.$route.params.type_id).then((response) => {
-            this.type = response.data;
-        })
-            .catch((error) => {
-                this.$router.push({ name: 'not-found' });
-            });
+    methods: {
+        fetchType(endpoint = store.api.baseUrl + 'type/' + this.$route.params.type_id) {
+            axios.get(endpoint).then((response) => {
+                this.type = response.data;
+            })
+                .catch((error) => {
+                    this.$router.push({ name: 'not-found' });
+                });
+        }
 
     },
+
+    created() {
+        this.fetchType();
+    },
+
+    //beforeRouteUpdate va solo per per le page
+    beforeRouteUpdate(to) {
+        this.fetchType(store.api.baseUrl + 'type/' + to.params.type_id);
+        // console.log(to.params.type_id);
+    }
+
 };
 </script>
 
@@ -32,7 +44,7 @@ export default {
         <h1 class="my-5">Projects by type
             <span v-if="type && type.id" class="badge mx-1" :style="{ backgroundColor: type.color }">{{ type.name }}</span>
         </h1>
-        <ProjectList :type_id="type ? type.id : null" v-if="type" />
+        <ProjectList :key="type.id" :type_id="type ? type.id : null" v-if="type" />
         <!-- usa la lista -->
     </div>
 </template>
